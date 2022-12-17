8a367691c5442ac16386f8cb55823e51fbdbbb55
nurulamin092 <mnurulamin092@gmial.com>
2022-12-17 11:58:00 +0600

    cart slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,13 @@
+import cartSlice from "../features/cart/cartSlice";
+import filterSlice from "../features/filter/filterSlice";
+
 const { configureStore } = require("@reduxjs/toolkit");
 
 const store = configureStore({
-    reducer:{}
+    reducer:{
+        cart: cartSlice,
+        filter: filterSlice
+    }
 })
 
 export default store;
