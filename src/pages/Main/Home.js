483965ada1b805495bd65eac08a168d3a2fdae10
nurulamin092 <mnurulamin092@gmial.com>
2022-12-17 20:52:03 +0600

    removeFromCart

--- src/pages/Main/Home.js
+++ src/pages/Main/Home.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import ProductCard from "../../components/ProductCard";
+import { toggle, toggleBrand } from "../../features/filter/filterSlice";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
@@ -10,20 +12,23 @@ const Home = () => {
       .then((data) => setProducts(data.data));
   }, []);
 
+  const dispatch = useDispatch();
+
   const activeClass = "text-white  bg-indigo-500 border-white";
 
   return (
     <div className='max-w-7xl gap-14 mx-auto my-10'>
       <div className='mb-10 flex justify-end gap-5'>
         <button
+        onClick={()=>dispatch(toggle())}
           className={`border px-3 py-2 rounded-full font-semibold ${activeClass} `}
         >
           In Stock
         </button>
-        <button className={`border px-3 py-2 rounded-full font-semibold`}>
+        <button  onClick={()=> dispatch (toggleBrand("amd"))} className={`border px-3 py-2 rounded-full font-semibold`}>
           AMD
         </button>
-        <button className={`border px-3 py-2 rounded-full font-semibold`}>
+        <button onClick={()=> dispatch(toggleBrand('intel'))} className={`border px-3 py-2 rounded-full font-semibold`}>
           Intel
         </button>
       </div>
