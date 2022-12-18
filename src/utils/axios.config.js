5ab36e2c05c5dbe9ccdde30a187708709b461562
nurulamin092 <mnurulamin092@gmial.com>
2022-12-18 10:25:10 +0600

    add axios config

--- src/utils/axios.config.js
+++ src/utils/axios.config.js
@@ -1,8 +1,25 @@
 import axios from "axios";
 
-// const url = `http://localhost:4000/`;
+let URL;
+//  const url = `http://localhost:4000/`;
+
+switch (process.env.REACT_APP_ENVIRONMENT) {
+    case "DEVELOPMENT":
+        URL="http://localhost:4000/";
+    
+    break
+    
+    case "PRODUCTION":
+        URL="production.com";
+        
+        break;
+
+    default:
+        URL="http://localhost:4000/";
+}
 const instance = axios.create({
-    baseURL: "http://localhost:4000/",
+    // baseURL: "http://localhost:4000/",
+    baseURL:URL
 });
 
 export default instance;
